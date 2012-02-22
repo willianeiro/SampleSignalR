c0484862bd66d9b80b3df3d046de3d1fd7aa2fc6
Willian Eiro <willianeiro@gmail.com>
2012-02-22 13:01:48 -0200

    First commit...

---
+++ SampleSignalR/Chat/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SampleSignalR.Chat
+{
+    public class Chat : Hub
+    {
+        public void Send(string mensagem)
+        {
+            Clients.addMessage(mensagem);
+        }
+    }
+}
